c8506a0fd928b13a317a64cbcdcd0fc47d60f8f9
swayam-kothekar <swayamkothekar52@gmail.com>
2024-12-20 13:30:43 +0530

    minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 import { Providers } from "@/components/Providers";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,11 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={cn(inter.className, "min-h-screen antialiased")}>
         <Providers>
-        <main className="h-screen dark text-foreground bg-background">{children}</main>
+          <main className="h-screen dark text-foreground bg-background">{children}</main>
         </Providers>
       </body>
     </html>
