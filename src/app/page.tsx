e020dbe8607785045aa83110c0f0dff77740a29a
swayam-kothekar <swayamkothekar52@gmail.com>
2024-12-26 12:26:31 +0530

    minor updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -125,7 +125,7 @@ const LandingPage = () => {
 
       <footer className="bg-gray-950 py-12">
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 text-center text-gray-400">
-          <p>© 2024 WebChat</p>
+          <p>© 2024 ChatWeb</p>
         </div>
       </footer>
     </div>
