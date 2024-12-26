c5fcf025147d7300e474656cc8dfe93f1e5abc54
swayam-kothekar <swayamkothekar52@gmail.com>
2024-12-26 13:04:10 +0530

    changed LLM for chatbot

--- src/lib/rag-chat.ts
+++ src/lib/rag-chat.ts
@@ -3,7 +3,7 @@ import { redis } from "./redis";
 
 export const ragChat = new RAGChat(
     {
-    model: upstash("meta-llama/Meta-Llama-3-8B-Instruct"),
+    model: upstash("mistralai/Mistral-7B-Instruct-v0.2"),
     redis: redis
 }
 )
